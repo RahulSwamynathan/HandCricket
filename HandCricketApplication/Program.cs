90cb9f25c174f7cad42479d17db401d31078a69a
RahulSwamynathan <rahulswa.rahul60@gmail.com>
2019-01-30 15:02:42 +0530

    Another change

--- HandCricketApplication/Program.cs
+++ HandCricketApplication/Program.cs
@@ -12,7 +12,7 @@ namespace HandCricketApplication
                                           "User2",
                                           "User3",
                                           "User4",
-                                          "User5");
+                                          "User6");
             handling.GetIndividualScore();
             handling.GetNoOfWins();
         }
